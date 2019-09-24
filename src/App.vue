25a2112a6cfe76d720bbf152a1cad4947a098811
= <=>
2019-09-24 12:02:24 -0400

    delete

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,15 @@
     <Header />
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-on:del-todo='deleteTodo' v-bind:todos="todos"/>
+    <Todos v-on:del-todo="deleteTodo" v-bind:todos="todos" />
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos'
-import Header from './components/layout/Header';
-import AddTodo from './components/AddTodo'
-import axios from 'axios';
+import Todos from "./components/Todos";
+import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
+import axios from "axios";
 export default {
   name: "app",
   components: {
@@ -25,20 +25,42 @@ export default {
     };
   },
   methods: {
-    deleteTodo(id){
-      this.todos = this.todos.filter(todo => todo.id !== id)
+    deleteTodo(id) {
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res => {
+          this.todos = this.todos.filter(todo => todo.id !== id);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     },
-    addTodo(newTodo){
-      this.todos = [...this.todos, newTodo];
+    addTodo(newTodo) {
+      // this.todos = [...this.todos, newTodo];
+      const { title, completed } = newTodo;
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed
+        })
+        .then(response => {
+          this.todos = [...this.todos, response.data];
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   },
-  created(){
-    axios.get('https://jsonplaceholder.typicode.com/todos').then(res => {
-      this.todos = res.data;
-      console.log(res.data);
-    }).catch(err => {
-      console.log(error);
-    })
+  created() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then(res => {
+        this.todos = res.data;
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(error);
+      });
   }
 };
 </script>
