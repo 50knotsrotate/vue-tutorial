22b5fed93c162121c6f2898e456aea3d9430c858
= <=>
2019-09-24 08:58:09 -0400

    todo component

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,51 @@
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  components: {
-    
+  name: "app",
+  components: {},
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo one",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo two",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Take out trash",
+          complted: false
+        },
+        {
+          id: 4,
+          title: "Walk dogs",
+          complted: false
+        },
+        {
+          id: 5,
+          title: "Go to work",
+          completed: false
+        },
+        {
+          id: 6,
+          title,'Drive home',
+          completed: false
+        }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Todos.vue
@@ -0,0 +1,12 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: "Todos"
+}
+</script>
+
+<style scoped>
+</style>
